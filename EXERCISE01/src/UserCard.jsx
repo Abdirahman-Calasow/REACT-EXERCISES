1eb82dc44a1c50296d7cda322d7d8f3053d34dc5
Abdirahman-Calasow <mmsomtech@gmail.com>
2026-09-13 01:58:50 -0700

    Fix EXERCISE01 folder structure

---
+++ EXERCISE01/src/UserCard.jsx
@@ -0,0 +1,12 @@
+const UserCard=()=>{
+    return(
+  <div>
+        <h1>mr: Calasow</h1>
+        <p>calasow is a softwer developer at mm somtech</p>
+    </div>
+    )
+  
+
+}
+
+export default UserCard;
